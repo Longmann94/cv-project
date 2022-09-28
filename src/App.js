ef472d31c3c9750657688765d34f96f715ff6c49
Long Mann <layfon@live.com.au>
2022-09-28 12:54:46 +1000

    change class components to function components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import reactDOM from 'react-dom';
 import General from './components/General';
 import Education from './components/Education';
@@ -7,293 +7,89 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/styles.css';
 import uniqid from 'uniqid';
-import { Formik, Field, Form } from 'formik';
 
 
-class App extends React.Component{
-  constructor() {
-    super();
+//const [item, setItem] = use.state;
+//use effect syntx:
+//useEffect(() => {}, [])
 
-    this.state = {
-      generalArr: [],
-      generalInfo: {
-        id: uniqid(),
-        name: '',
-        email: '',
-        phone: '',
-      },
+const App = () => {
 
-      educationArr: [],
-      educationInfo: {
-        id: uniqid(),
-        school: '',
-        titleOfStudy: '',
-        dateOfStudy: '',
-      },
+    const [generalArr, setGeneralArr] = useState([]);
+    const [generalInfo, setGeneralInfo] = useState({
+      name: '',
+      email: '',
+      phone: '',
+    });
 
-      practicalArr: [],
-      practicalInfo: {
-        id: uniqid(),
-        company: '',
-        position: '',
-        title: '',
-        dateOfWork: '',
-        details: '',
-      },
+    const handleNameInputChange = (e) => {
+      e.persist();
+      setGeneralInfo((values) => ({
+        ...values,
+        name: e.target.value,
+      }));
+    };
+
+    const handleEmailInputChange = (e) => {
+      e.persist();
+      setGeneralInfo((values) => ({
+        ...values,
+        email: e.target.value,
+      }));
+    };
+
+    const handlePhoneInputChange = (e) => {
+      e.persist();
+      setGeneralInfo((values) => ({
+        ...values,
+        phone: e.target.value,
+      }));
+    };
+
+    const handleSubmitGeneralInfo = () => {
+      setGeneralArr(
+        [
+          ...generalArr,
+          {id: uniqid(), item: generalInfo}
+        ]
+      );
     }
 
-   this.handleInputChangeGeneralInfo = this.handleInputChangeGeneralInfo.bind(this);
-   this.handleSubmitGeneralInfo = this.handleSubmitGeneralInfo.bind(this);
 
-   this.handleInputChangeEducationInfo = this.handleInputChangeEducationInfo.bind(this);
-   this.handleSubmitEducationInfo = this.handleSubmitEducationInfo.bind(this);
-
-   this.handleInputChangePracticalInfo = this.handleInputChangePracticalInfo.bind(this);
-   this.handleSubmitPracticalInfo = this.handleSubmitPracticalInfo.bind(this);
-
-   this.handleEditLastEntryGeneral = this.handleEditLastEntryGeneral.bind(this);
-   this.handleEditLastEntryEducation = this.handleEditLastEntryEducation.bind(this);
-   this.handleEditLastEntryPractical = this.handleEditLastEntryPractical.bind(this);
-  };
-
-  handleInputChangeGeneralInfo(values){
-    this.setState({
-      generalInfo: {
-        id: uniqid(),
-        name: values.name,
-        email: values.email,
-        phone: values.phone,
-      },
+    const [educationArr, setEducationArr] = useState([]);
+    const [educationInfo, setEducationInfo] = useState({
+      id: uniqid(),
+      school: '',
+      titleOfStudy: '',
+      dateOfStudy: '',
     });
-  }
 
-  handleSubmitGeneralInfo(){
-     this.setState({
-       generalArr: this.state.generalArr.concat(this.state.generalInfo),
-       generalInfo: {
-         id: uniqid(),
-         name: '',
-         email: '',
-         phone: '',
-       }
-     });
-  }
-
-  handleEditLastEntryGeneral(){
-    if(this.state.generalArr.length !== 0){
-      const lastEntry = this.state.generalArr.pop();
-
-    this.setState({
-      generalArr: this.state.generalArr.slice(0, this.state.generalArr.length),
-      generalInfo: {
-        id: uniqid(),
-        name: lastEntry.name,
-        email: lastEntry.email,
-        phone: lastEntry.phone,
-      }
+    const [practicalArr, setPracticalArr] = useState([]);
+    const [practicalInfo, setPracticalInfo] = useState({
+      id: uniqid(),
+      company: '',
+      position: '',
+      title: '',
+      dateOfWork: '',
+      details: '',
     });
-  }
-  }
-
-  handleInputChangeEducationInfo(values){
-    this.setState({
-      educationInfo: {
-        id: uniqid(),
-        school: values.school,
-        titleOfStudy: values.titleOfStudy,
-        dateOfStudy: values.dateOfStudy,
-      },
-    });
-  }
-
-  handleSubmitEducationInfo(){
-     this.setState({
-       educationArr: this.state.educationArr.concat(this.state.educationInfo),
-       educationInfo: {
-         id: uniqid(),
-         school: '',
-         titleOfStudy: '',
-         dateOfStudy: '',
-       },
-     });
-  }
-
-  handleEditLastEntryEducation(){
-    if(this.state.educationArr.length !== 0){
-      const lastEntry = this.state.educationArr.pop();
-
-    this.setState({
-      educationArr: this.state.educationArr.slice(0, this.state.educationArr.length),
-      educationInfo: {
-        id: uniqid(),
-        school: lastEntry.school,
-        titleOfStudy: lastEntry.titleOfStudy,
-        dateOfStudy: lastEntry.dateOfStudy,
-      }
-    });
-  }
-  }
-
-  handleInputChangePracticalInfo(values){
-    this.setState({
-      practicalInfo: {
-        id: uniqid(),
-        company: values.company,
-        position: values.position,
-        title: values.title,
-        dateOfWork: values.dateOfWork,
-        details: values.details,
-      },
-    });
-  }
-
-  handleSubmitPracticalInfo(){
-     this.setState({
-       practicalArr: this.state.practicalArr.concat(this.state.practicalInfo),
-       practicalInfo: {
-         id: uniqid(),
-         company: '',
-         position: '',
-         title: '',
-         dateOfWork: '',
-         details: '',
-       },
-     });
-  }
-
-  handleEditLastEntryPractical(){
-    if(this.state.practicalArr.length !== 0){
-      const lastEntry = this.state.practicalArr.pop();
-
-    this.setState({
-      practicalArr: this.state.practicalArr.slice(0, this.state.practicalArr.length),
-      practicalInfo: {
-        id: uniqid(),
-        company: lastEntry.company,
-        position: lastEntry.position,
-        title: lastEntry.title,
-        dateOfWork: lastEntry.dateOfWork,
-        details: lastEntry.details,
-      }
-    });
-  }
-  }
-
-
-  render(){
-
-    const { generalArr, generalInfo, educationArr, educationInfo, practicalArr, practicalInfo } = this.state;
 
     return (
         <div className="main-container">
           <Header />
-          <General generalArr={generalArr}/>
+          <General generalArr={generalArr} />
         <div className="general-form">
           <h2>Enter your general information</h2>
-              <Formik
-               enableReinitialize = {true}
-               initialValues={{
-                name: generalInfo.name,
-                email: generalInfo.email,
-                phone: generalInfo.phone,
-               }}
-              onSubmit={async (values, {resetForm}) => {
-                await this.handleInputChangeGeneralInfo(values);
-                this.handleSubmitGeneralInfo();
-                resetForm({ values: ''});
-              }}
-              >
-              <Form>
-               <label htmlFor="name">Name:</label>
-               <Field id="name" name="name" placeholder="Name" />
-
-               <label htmlFor="email">Email:</label>
-               <Field id="email" name="email" placeholder="Email" />
-
-                <label htmlFor="phone">Phone: </label>
-                <Field id="phone" name="phone" placeholder="Phone number" />
-
-                <button type="submit"> Add details</button>
-                <button type="button" onClick={this.handleEditLastEntryGeneral}>Edit Last entry </button>
-               </Form>
-               </Formik>
+            <label htmlFor="name">Name:</label>
+            <input id="name" type="text" placeholder="Name..." id="name" value={generalInfo.name} onChange={handleNameInputChange}/>
+            <label htmlFor="email">Email:</label>
+            <input id="email" type="text" placeholder="email..." id="email" value={generalInfo.email} onChange={handleEmailInputChange}/>
+            <label htmlFor="phone">Phone:</label>
+            <input id="phone" type="text" placeholder="phone..." id="phone" value={generalInfo.phone} onChange={handlePhoneInputChange}/>
+            <button onClick={handleSubmitGeneralInfo}>submit Info!</button>
           </div>
-          <Education educationArr={educationArr}/>
-          <div className="education-form">
-            <h2>Enter your Education information</h2>
-                <Formik
-                  enableReinitialize = {true}
-                  initialValues={{
-                  school: educationInfo.school,
-                  titleOfStudy: educationInfo.titleOfStudy,
-                  dateOfStudy: educationInfo.dateOfStudy,
-                }}
-                onSubmit={async (values, {resetForm}) => {
-                  await this.handleInputChangeEducationInfo(values);
-                  this.handleSubmitEducationInfo();
-                  resetForm({ values: ''});
-                }}
-                >
-                <Form>
-                 <label htmlFor="school">School:</label>
-                 <Field id="school" name="school" placeholder="School Name" />
+          </div>
+        );
+    }
 
-                 <label htmlFor="titleOfStudy">Title of Study:</label>
-                 <Field id="titleOfStudy" name="titleOfStudy" placeholder="...Master of IT" />
-
-                  <label htmlFor="dateOfStudy">Date of Study: </label>
-                  <Field id="dateOfStudy" name="dateOfStudy" placeholder="Year Completed" />
-
-                  <button type="submit"> Add details</button>
-                  <button type="button" onClick={this.handleEditLastEntryEducation}>Edit Last entry </button>
-                 </Form>
-                 </Formik>
-            </div>
-          <Practical practicalArr={practicalArr}/>
-          <div className="practical-form">
-            <h2>Enter your Practical information</h2>
-                <Formik
-                  enableReinitialize = {true}
-                  initialValues={{
-                  company: practicalInfo.company,
-                  position: practicalInfo.position,
-                  title: practicalInfo.title,
-                  dateOfWork: practicalInfo.dateOfWork,
-                  details: practicalInfo.details,
-                }}
-                onSubmit={async (values, {resetForm}) => {
-                  await this.handleInputChangePracticalInfo(values);
-                  this.handleSubmitPracticalInfo();
-                  resetForm({ values: ''});
-                }}
-                >
-                <Form>
-                 <label htmlFor="company">Company:</label>
-                 <Field id="company" name="company" placeholder="Company name" />
-
-                 <label htmlFor="position">Position:</label>
-                 <Field id="position" name="position" placeholder="Position" />
-
-                 <label htmlFor="title">Title: </label>
-                 <Field id="title" name="title" placeholder="Title" />
-
-                  <label htmlFor="dateOfWork">Date of Work: </label>
-                  <Field id="dateOfWork" name="dateOfWork" placeholder="Start...End Date" />
-
-                  <label htmlFor="details">Details: </label>
-                  <Field id="details" name="details" placeholder="Details..." />
-
-                  <button type="submit"> Add details</button>
-                  <button type="button" onClick={this.handleEditLastEntryPractical}>Edit Last entry </button>
-                 </Form>
-                 </Formik>
-            </div>
-          <Footer />
-        </div>
-    );
-  }
-
-
-}
-
-export default App;
+    export default App;
--- src/components/General.js
+++ src/components/General.js
@@ -1,28 +1,25 @@
 import React from 'react';
 
-class General extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const General = ({ generalArr }) => {
 
-
-  render() {
-    const generalArr = this.props.generalArr;
-    const listItems = generalArr.map((item) =>
-      <div className="general-content" key={item.id}>
-      <div className="information-label">Name:</div><div className="information-data">{item.name}</div>
-      <div className="information-label">Email:</div><div className="information-data">{item.email}</div>
-      <div className="information-label">Phone:</div><div className="information-data">{item.phone}</div>
-      </div>
-  );
+   console.log(Array.isArray(generalArr));
+   console.log(generalArr);
+   const listItems = generalArr.map(item => {
+     console.log(item);
+    return <div className="general-content" key={item.id}>
+     <div className="information-label">Name:</div><div className="information-data">{item.item.name}</div>
+     <div className="information-label">Email:</div><div className="information-data">{item.item.email}</div>
+     <div className="information-label">Phone:</div><div className="information-data">{item.item.phone}</div>
+     </div>;
+ });
 
     return (
       <div className="general-section">
       <div className="section-title"> General Information: </div>
-        {listItems}
+      {listItems}
       </div>
     );
-  }
+
 }
 
 export default General;
